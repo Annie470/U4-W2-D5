085b0d4d954d77fc1077e9db2340fc820fbc54e6
Anna De Lise <anna_delise@libero.it>
2025-09-19 16:21:57 +0200

    add ex and changes

--- src/main/java/u4w2d5/Application.java
+++ src/main/java/u4w2d5/Application.java
@@ -1,6 +1,7 @@
 package u4w2d5;
 
 import u4w2d5.entities.*;
+import u4w2d5.exceptions.InputNonValido;
 import u4w2d5.exceptions.NumeroGiocatoriNonConforme;
 
 import java.time.LocalDate;
@@ -16,7 +17,7 @@ public class Application {
         Scanner scanner = new Scanner(System.in);
 
 
-        //inserisco almeno due elementi con il secondo costruttore con id senza count
+        //inserisco almeno due2 elementi con il secondo costruttore con id senza count
         Videogioco v1= new Videogioco(1, "MonkeyIsland", LocalDate.of(1990, 1,1), 15, Piattaforma.PC, 130, Genere.POINTCLICK  );
         giochi.add(v1);
         GiocoTavolo g1 = new GiocoTavolo(2, "Kragmortha", LocalDate.of(2007, 3,1), 25, 10,20);
@@ -37,38 +38,59 @@ public class Application {
             System.out.println("3 -> RICERCA PER PREZZO ");
             System.out.println("4 -> RICERCA PER NUMERO GIOCATORI ");
             System.out.println("5 -> RIMUOVERE GIOCO ");
+            System.out.println("6 -> STATISTICHE COLLEZIONE ");
             System.out.println("9 -> TERMINA ");
 
             int scelta = Integer.parseInt(scanner.nextLine());
-            switch (scelta){
-                case 1:
-                    collezione.aggiungereElemento(giochi);
-                    break;
-                case 2:
-                    System.out.println("Inserisci ID");
-                    int id = Integer.parseInt(scanner.nextLine());
-                    collezione.ricercarePerID(id, giochi);
-                    break;
-                case 3:
-                    System.out.println("Inserisci prezzo");
-                    double prezzo = Double.parseDouble(scanner.nextLine());
-                    collezione.ricercarePerPrezzo(prezzo, giochi);
-                    break;
-                case 4:
-                    System.out.println("Inserisci num giocatori");
-                    int nGiocat = Integer.parseInt(scanner.nextLine());
-                    collezione.ricercarePerGiocatori(nGiocat, giochi);
-                    break;
-                case 5:
-                    System.out.println("Inserisci ID");
-                    int idR = Integer.parseInt(scanner.nextLine());
-                    collezione.rimuovereGioco( idR, giochi);
-                    break;
-                case 9:
-                    valoreWhile = true;
-                    break;
-                default: System.out.println("Scelta non valida");
-            }
+            try {
+                switch (scelta){
+                    case 1:
+                        collezione.aggiungereElemento(giochi);
+                        break;
+                    case 2:
+                        System.out.println("Inserisci ID");
+                        try {
+                            int id = Integer.parseInt(scanner.nextLine());
+                            collezione.ricercarePerID(id, giochi);
+                        } catch (NumberFormatException e) {
+                            throw new InputNonValido("ID solo numerico!");
+                        }
+                        break;
+                    case 3:
+                        System.out.println("Inserisci prezzo");
+                        double prezzo = Double.parseDouble(scanner.nextLine());
+                        if(prezzo >=1){
+                            collezione.ricercarePerPrezzo(prezzo, giochi);
+                            break;
+                        } else {
+                            throw new InputNonValido("Il prezzo non può essere negativo!");
+                        }
+                    case 4:
+                        System.out.println("Inserisci num giocatori");
+                        int nGiocat = Integer.parseInt(scanner.nextLine());
+                        if(nGiocat >= 2 && nGiocat <= 10){
+                            collezione.ricercarePerGiocatori(nGiocat, giochi);
+                            break;
+                        } else {
+                            throw  new InputNonValido("Il numero dei giocatori deve essere tra 2-10");
+                        }
+                    case 5:
+                        System.out.println("Inserisci ID");
+                        int idR = Integer.parseInt(scanner.nextLine());
+                        collezione.rimuovereGioco(idR, giochi);
+                        break;
+                    case 6:
+                        collezione.stampareStatistiche(giochi);
+                        break;
+                    case 9:
+                        valoreWhile = true;
+                        break;
+                    default:
+                        System.out.println("Scelta non valida");
+                }
+            } catch (InputNonValido e) {
+            System.out.println(e.getMessage());
+        }
 
 
         }
--- src/main/java/u4w2d5/entities/Collezione.java
+++ src/main/java/u4w2d5/entities/Collezione.java
@@ -5,6 +5,7 @@ import u4w2d5.exceptions.NumeroGiocatoriNonConforme;
 
 import java.time.LocalDate;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Collezione {
     private List<Gioco> giochi = new ArrayList<>();
@@ -28,6 +29,10 @@ public class Collezione {
                     case "V": {
                         System.out.println("Inserisci titolo: ");
                         String titolo = scanner.nextLine();
+                        boolean compTitolo = giochi.stream().map(Gioco::getTitolo).anyMatch(titoloFiltrato -> titoloFiltrato.trim().equalsIgnoreCase(titolo.trim())); // avevo inserito toLowerCase(), intellij propone equalsIgnoreCase
+                        if (compTitolo){
+                            throw new InputNonValido("Titolo gia inserito");
+                        }
 
                         System.out.println("Inserisci anno di pubblicazione es(2025-10-03): ");
                         LocalDate annoPubblicazione;
@@ -41,8 +46,11 @@ public class Collezione {
                         double prezzo;
                         try {
                             prezzo = Double.parseDouble(scanner.nextLine());
+                            if (prezzo <= 0) {
+                                throw new InputNonValido("Il prezzo deve essere un numero positivo");
+                            }
                         } catch (NumberFormatException e) {
-                            throw new InputNonValido("SOLO NUMERI INTERI O CON LA VIRGOLA");
+                            throw new InputNonValido("SOLO NUMERI");
                         }
 
                         System.out.println("Inserisci piattaforma tra ps4, ps5, xbox, pc, switch: ");
@@ -57,6 +65,9 @@ public class Collezione {
                         double durataGioco;
                         try {
                             durataGioco = Double.parseDouble(scanner.nextLine());
+                            if (durataGioco <= 0) {
+                                throw new InputNonValido("La durata gioco deve esseere maggiore di 0");
+                            }
                         } catch (NumberFormatException e) {
                             throw new InputNonValido("SOLO NUMERI");
                         }
@@ -78,6 +89,10 @@ public class Collezione {
                     case "GT": {
                         System.out.println("Inserisci titolo: ");
                         String titolo = scanner.nextLine();
+                        boolean compTitolo = giochi.stream().map(Gioco::getTitolo).anyMatch(titoloFiltrato -> titoloFiltrato.trim().equalsIgnoreCase(titolo.trim())); // avevo inserito toLowerCase(), intellij propone equalsIgnoreCase
+                        if (compTitolo){
+                            throw new InputNonValido("Titolo gia inserito");
+                        }
 
                         System.out.println("Inserisci anno di pubblicazione es(2025-10-03): ");
                         LocalDate annoPubblicazione;
@@ -91,14 +106,20 @@ public class Collezione {
                         double prezzo;
                         try {
                             prezzo = Double.parseDouble(scanner.nextLine());
+                            if (prezzo <= 0) {
+                                throw new InputNonValido("Il prezzo deve essere un numero positivo");
+                            }
                         } catch (NumberFormatException e) {
-                            throw new InputNonValido("SOLO NUMERI INTERI O CON LA VIRGOLA");
+                            throw new InputNonValido("SOLO NUMERI");
                         }
 
                         System.out.println("Inserisci numero giocatori da 2 a 10: ");
                         int numeroGiocatori;
                         try {
                             numeroGiocatori = Integer.parseInt(scanner.nextLine());
+                            if (numeroGiocatori < 2 || numeroGiocatori > 10) {
+                                throw new InputNonValido("Numero giocatori deve essere tra 2 e 10");
+                            }
                         } catch (NumberFormatException e) {
                             throw new InputNonValido("NUMERO GIOCATORI NON VALIDO");
                         }
@@ -107,6 +128,9 @@ public class Collezione {
                         double durataPartita;
                         try {
                             durataPartita = Double.parseDouble(scanner.nextLine());
+                            if (durataPartita <= 0) {
+                                throw new InputNonValido("La durata gioco deve esseere maggiore di 0");
+                            }
                         } catch (NumberFormatException e) {
                             throw new InputNonValido("INSERISCI SOLO NUMERI");
                         }
@@ -165,6 +189,22 @@ public class Collezione {
                 System.out.println(gioco.getTitolo() + " rimosso");
             }
         }
+    }
+
+    public void stampareStatistiche(List<Gioco> listaGiochi){
+        /*int sizeT = listaGiochi.size();
+        int sizeV = listaGiochi.stream().filter(gioco -> gioco instanceof Videogioco).toList().size();
+        String nG=  sizeT - sizeV + " Giochi da tavolo e " + sizeV + " Videogiochi";*/
+        Optional<Gioco> gP = listaGiochi.stream().max(Comparator.comparing(Gioco::getPrezzo)); // avevo inserito .sorted(Comparator.comparing(Gioco::getPrezzo).reversed()).findFirst();
+        /*double sGP = listaGiochi.stream().mapToDouble(Gioco::getPrezzo).sum(); // avevo inserito .collect(Collectors.summingDouble(Gioco::getPrezzo));
+        double mG = sGP / sizeT;
+        if(!listaGiochi.isEmpty() && gP.isPresent()){
+            System.out.println(nG + "\n" + "Gioco più caro : " + gP.get().titolo + " "+ gP.get().getPrezzo() +"\n" + " Media prezzo: " + mG);
+        }*/
+        //HO IMPIEGATO TROPPO TEMPO PER POTERLO ELIMINARE.... HO RILETTO GLI APPUNTI -.-''
+
+       DoubleSummaryStatistics stats = listaGiochi.stream().collect(Collectors.summarizingDouble(Gioco::getPrezzo));
+        System.out.println("Giochi: " + stats.getCount() + "\nGioco piu caro: " + gP.get().getTitolo() + " - " + stats.getMax() + "\nMedia prezzo: " + stats.getAverage() );
     }
 
     public void setGiochi(List<Gioco> giochi) {
